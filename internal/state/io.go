c76ae311367fe31451d5d86669e5aa4721d71a75
Volkan Özçelik <me@volkan.io>
2023-01-27 16:11:39 -0800

    backup function (#11)
    * backup function
    
    * minor

--- internal/state/io.go
+++ internal/state/io.go
@@ -16,8 +16,10 @@ import (
 	"github.com/zerotohero-dev/aegis-core/env"
 	"github.com/zerotohero-dev/aegis-core/log"
 	"io"
+	"math"
 	"os"
 	"path"
+	"strconv"
 	"strings"
 )
 
@@ -31,18 +33,18 @@ func ageKeyPair() (string, string) {
 	return parts[0], parts[1]
 }
 
-func readFromDisk(key string) *entity.SecretStored {
+func decryptDataFromDisk(key string) ([]byte, error) {
 	dataPath := path.Join(env.SafeDataPath(), key+".age")
 
 	if _, err := os.Stat(dataPath); os.IsNotExist(err) {
-		log.TraceLn("readFromDisk: No file at:", dataPath)
-		return nil
+		log.TraceLn("decryptDataFromDisk: No file at:", dataPath)
+		return nil, err
 	}
 
 	data, err := os.ReadFile(dataPath)
 	if err != nil {
-		log.WarnLn("readFromDisk: Error reading file:", err.Error())
-		return nil
+		log.WarnLn("decryptDataFromDisk: Error reading file:", err.Error())
+		return nil, err
 	}
 
 	privateKey, _ := ageKeyPair()
@@ -50,11 +52,12 @@ func readFromDisk(key string) *entity.SecretStored {
 	identity, err := age.ParseX25519Identity(privateKey)
 	if err != nil {
 		log.WarnLn("Failed to parse private key", privateKey, err)
+		return nil, err
 	}
 
 	if len(data) == 0 {
 		log.WarnLn("file on disk appears to be empty")
-		return nil
+		return nil, err
 	}
 
 	out := &bytes.Buffer{}
@@ -63,15 +66,22 @@ func readFromDisk(key string) *entity.SecretStored {
 	r, err := age.Decrypt(f, identity)
 	if err != nil {
 		log.WarnLn("Failed to open encrypted file", err.Error())
-		return nil
+		return nil, err
 	}
 
 	if _, err := io.Copy(out, r); err != nil {
 		log.WarnLn("Failed to read encrypted file", err.Error())
-		return nil
+		return nil, err
 	}
 
-	contents := out.Bytes()
+	return out.Bytes(), nil
+}
+
+func readFromDisk(key string) *entity.SecretStored {
+	contents, err := decryptDataFromDisk(key)
+	if err != nil {
+		return nil
+	}
 
 	var secret entity.SecretStored
 	err = json.Unmarshal(contents, &secret)
@@ -82,24 +92,15 @@ func readFromDisk(key string) *entity.SecretStored {
 	return &secret
 }
 
-func persist(secret entity.SecretStored) {
-	// Resetting the value also removes the secret file from the disk.
-	if secret.Value == "" {
-		dataPath := path.Join(env.SafeDataPath(), secret.Name+".age")
-		err := os.Remove(dataPath)
-		if !os.IsNotExist(err) {
-			log.WarnLn("persist: failed to remove secret", err.Error())
-		}
-		return
-	}
+var lastBackedUpIndex = make(map[string]int)
 
+func saveSecretToDisk(secret entity.SecretStored, dataPath string) {
 	data, err := json.Marshal(secret)
 	if err != nil {
 		log.WarnLn("persist: failed to marshal secret", err.Error())
 		return
 	}
 
-	dataPath := path.Join(env.SafeDataPath(), secret.Name+".age")
 	file, err := os.Create(dataPath)
 	if err != nil {
 		log.WarnLn("persist: problem creating file", err.Error())
@@ -134,4 +135,41 @@ func persist(secret entity.SecretStored) {
 			log.InfoLn("problem closing stream", err.Error())
 		}
 	}()
+}
+
+// Only one goroutine accesses this function at any given time.
+func persist(secret entity.SecretStored) {
+	backupCount := env.SafeSecretBackupCount()
+
+	// Resetting the value also removes the secret file from the disk.
+	if secret.Value == "" {
+		dataPath := path.Join(env.SafeDataPath(), secret.Name+".age")
+		err := os.Remove(dataPath)
+		if !os.IsNotExist(err) {
+			log.WarnLn("persist: failed to remove secret", err.Error())
+		}
+		return
+	}
+
+	// Save the secret
+	dataPath := path.Join(env.SafeDataPath(), secret.Name+".age")
+	saveSecretToDisk(secret, dataPath)
+
+	index, found := lastBackedUpIndex[secret.Name]
+	if !found {
+		lastBackedUpIndex[secret.Name] = 0
+		index = 0
+	}
+
+	newIndex := math.Mod(float64(index+1), float64(backupCount))
+
+	// Save a copy
+	dataPath = path.Join(
+		env.SafeDataPath(),
+		secret.Name+"-"+strconv.Itoa(int(newIndex))+"-"+".age",
+	)
+
+	saveSecretToDisk(secret, dataPath)
+
+	lastBackedUpIndex[secret.Name] = int(newIndex)
 }
